28351bee849c791723a49f53ffd10dd8d6f11a74
Scott Murray <smurray1@stevens.edu>
2020-12-13 13:38:23 -0500

    Added solid.handlebars

--- Solid-App/routes/solids.js
+++ Solid-App/routes/solids.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const solids = require("../data/solids");
+const users = require("../data/users");
+const comments = require("../data/comments");
 const uuid = require('uuid');
 
 function isValidString(string) {
@@ -22,7 +24,16 @@ router.get("/:id", async (req, res) => {
     }
     try {
         const solid = await solids.getSolidById(req.params.id);
-        res.status(200).json(result);
+        const creator = await users.getUserById(solid.postedBy);
+        let comments = [];
+        for (comment of solid.comments) {
+            comments.push(await comments.getCommentById(comment));
+        }
+        res.status(200).render("solids/solid.handlebars", {
+            solid: solid,
+            creator: creator,
+            comments: comments
+        });
     } catch (e) {
         if (e === 'solid not found') {
             res.status(404).json({error: "Solid not found"});
