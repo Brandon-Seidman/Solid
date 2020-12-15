3e3c8628d510a20dcbc735c17de3578a7e76103d
Jason Meyerberg <jmeyerbe@stevens.edu>
2020-12-15 13:26:17 -0500

    Database Adjustments

--- data/index.js
+++ data/index.js
@@ -1,11 +1,24 @@
+<<<<<<< HEAD
 const solidData = require("./solids");
 const userData = require("./users");
 const buddyData = require("./buddies");
 const commentData = require("./comments");
+=======
+const solidData = require('./solids');
+const userData = require('./users');
+//const buddyData = require('./buddies');
+const commentData = require('./comments');
+>>>>>>> Database Adjustments
 
 module.exports = {
   users: userData,
   solids: solidData,
+<<<<<<< HEAD
   buddies: buddyData,
   comments: commentData,
 };
+=======
+  //buddies: buddyData,
+  comments: commentData
+};
+>>>>>>> Database Adjustments
