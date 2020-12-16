4222424b8d569cfa9d708055c2a4a545b68bdb0c
Shannon Hobby <shannonhobby1@hotmail.com>
2020-12-16 13:50:28 -0500

    merge fix with search and posts

--- routes/mainview.js
+++ routes/mainview.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const solids = require("../data/solids");
+const data = require("../data");
+const solidData = data.solids;
 
 let authentication = async function (req, res, next) {
   let username = req.cookies.AuthCookie;
@@ -10,14 +11,61 @@ let authentication = async function (req, res, next) {
     next();
   }
 };
-
 router.use(authentication);
 router.get("/", async (req, res) => {
-  const solid = await solids.getAllSolids();
-  //console.log(solid);
   return res.render("solids/mainview.handlebars", {
-    title: "Home", solid: solid
+    title: "Home",
+    user: req.cookies.AuthCookie,
   });
 });
 
+router.post("/", async (req, res) => {
+  const {
+    location,
+    description,
+    postedBy,
+    accepted,
+    completed,
+    comments,
+    buddyID,
+    price,
+    timestamp,
+    tags,
+  } = req.body;
+  if (
+    !location ||
+    !description ||
+    !postedBy ||
+    accepted === null ||
+    completed === null ||
+    !comments ||
+    !price ||
+    !timestamp ||
+    !tags
+  )
+    throw "Error: All fields required";
+  if (
+    !location.trim() ||
+    !description.trim() ||
+    !postedBy.trim() ||
+    !Number.isInteger(price) ||
+    !timestamp.trim()
+  )
+    throw "Error: All fields require non-empty strings";
+
+  let newSolid = await solidData.addSolid(
+    location,
+    description,
+    postedBy,
+    accepted,
+    completed,
+    comments,
+    buddyID,
+    price,
+    timestamp,
+    tags
+  );
+
+  let solids = await solidData.getAllSolids();
+});
 module.exports = router;
