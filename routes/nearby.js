ef02fc6c952dbb5d0e1db22a3b656bfba311c1c5
Brandon-Seidman <43582959+Brandon-Seidman@users.noreply.github.com>
2020-12-17 12:27:09 -0500

    Added nearby and loc range func

---
+++ routes/nearby.js
@@ -0,0 +1,66 @@
+const mongoCollections = require('../config/mongoCollections');
+const connection = require('../config/mongoConnection');
+const express = require("express");
+const router = express.Router();
+let { ObjectId } = require('mongodb');
+const solids = require("../data/solids");
+const solidList = mongoCollections.solids;
+
+let authentication = async function (req, res, next) {
+  let username = req.cookies.AuthCookie;
+  if (!username) {
+    res.status(403).render("login/error.handlebars");
+  } else {
+    next();
+  }
+};
+
+router.use(authentication);
+router.get("/", async (req, res) => {
+  let username = req.cookies.AuthCookie;
+
+  const solid = await solidData.getSolidByUser(username);
+
+  return res.render("solids/account.handlebars", {
+	title: "Neaby Solids",
+  solid: solid
+  });
+});
+
+router.get("/", async (req, res) => {
+  //solid = await solids.getSolidByLocation('07030');
+  //console.log(solid);
+  res.render("solids/searchlo.handlebars", {
+    title: "Location Search",
+    //solid: solid
+  });
+  return;
+});
+
+router.post('/', async (req, res) => {
+  let sTerm = req.body.searchTerm;
+  try {
+    if (!sTerm || sTerm == '') {
+      throw 'Enter term';
+    }
+    solid = await solids.getSolidByLocation(sTerm);
+    if(solid.length == 0){
+      res.render("solids/searchlo.handlebars", {
+        title: "Location Search",
+        error: "No Solids Found."
+      });
+    } else {
+      res.render("solids/searchlo.handlebars", {
+        title: "Loaction Search",
+        solid: solid,
+      });
+    }
+  }catch(e){
+    res.status(404).render("solids/searchlo.handlebars", {
+      title: "Location Search",
+      error: "Error Finding Solids."
+    });
+  }
+});
+
+module.exports = router;
